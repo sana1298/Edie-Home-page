3fb38745e5c75fde277a827f68298194eb28d1cd
sana1298 <sanoferbavasa@gmail.com>
2023-05-22 19:32:45 +0530

    test

---
+++ home.js
@@ -0,0 +1,12 @@
+let navIcons = document.querySelector(".nav-menu-icon");
+let barIcon = document.querySelector(".fa-bars");
+let timesIcon = document.querySelector(".fa-times");
+let navMenuList = document.querySelector(".nav-menu-list");
+
+// Function to Toggle Icon when clicked
+navIcons.addEventListener("click", () => {
+	barIcon.classList.toggle("hidden");
+	timesIcon.classList.toggle("hidden");
+
+	navMenuList.classList.toggle("motion");
+});
